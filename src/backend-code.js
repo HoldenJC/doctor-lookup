2d93f676e9fbe7ee06df33f4966663d1dbc83bb4
HoldenJC <holdenj_c@yahoo.com>
2019-07-05 15:41:37 -0700

    add backend to retrieve address, landline phone number, website url, and patient acceptance from API and store in object

--- src/backend-code.js
+++ src/backend-code.js
@@ -1,77 +1,69 @@
 import $ from 'jquery';
-let docListTemp = [];
-let docListTemp2 = [];
 
 export function DoctorList() {
   this.doctors = [];
 }
 
-DoctorList.prototype.addDoctor = function(doc){
-  // debugger;
-  this.doctors = doc;
-  console.log(doc);
-}
+DoctorList.prototype.searchDoc = function (query) {
 
-DoctorList.prototype.searchDoc = function(query){
-
-  // debugger;
   let that = this;
-  // let docListTemp = [];
+  let docListTemp = [];
 
-  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=45.523064,-122.676483,50&skip=0&limit=10&user_key=${process.env.exports.apiKey}&query=${query}`).then(function(response){
+  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=45.523064,-122.676483,50&skip=0&limit=10&user_key=${process.env.exports.apiKey}&query=${query}`).then(function (response) {
 
-    // let that = this;
     let docArray = response.data;
-    // let foundDocTemp;
     console.log(docArray);
-    
+
 
     docArray.forEach((doctor) => {
-      // if(doctor.within_search_area === true){
-        let foundDoctor = new Doctor();
-        if(doctor.profile.first_name !== undefined){
-          foundDoctor.firstName = doctor.profile.first_name;
-        }
-        if(doctor.profile.last_name !== undefined){
-          foundDoctor.lastName = doctor.profile.last_name;
-        }
-        // foundDoctor.address = doctor.visit_address;
-        // (doctor.phones).forEach(function(phoneNumber){
-        //   if(phoneNumber.type === "landline"){
-        //     foundDoctor.phone = phoneNumber.number;
-        //   }
-        // });
-        if(doctor.website !== undefined){
-          foundDoctor.website = doctor.website;
-        }
-        if(doctor.accepts_new_patients !== undefined){
-          foundDoctor.acceptsPatients = doctor.accepts_new_patients;
-        }
-        
 
-        console.log(foundDoctor);
-         
-        docListTemp.push(foundDoctor); 
+      let foundDoctor = new Doctor();
+      if (doctor.profile.first_name !== undefined) {
+        foundDoctor.firstName = doctor.profile.first_name;
+      }
+      if (doctor.profile.last_name !== undefined) {
+        foundDoctor.lastName = doctor.profile.last_name;
+      }
+      if (doctor.practices[0].visit_address !== undefined){
+        foundDoctor.address = `${doctor.practices[0].visit_address.street}, ${doctor.practices[0].visit_address.city}. ${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.zip}`;
+      }
+      if (doctor.practices[0].phones !== undefined){
+        (doctor.practices[0].phones).forEach(function(phoneNumber){
+          if(phoneNumber.type === "landline"){
+            foundDoctor.phone = phoneNumber.number;
+          }
+        });
+      }
+      if (doctor.practices[0].website !== undefined) {
+        foundDoctor.website = doctor.practices[0].website;
+      }
+      if (doctor.practices[0].accepts_new_patients !== undefined) {
+        foundDoctor.acceptsPatients = doctor.practices[0].accepts_new_patients;
+      }
 
-        docListTemp2 = docListTemp.slice();
-        
-        console.log(docListTemp);
 
-      // }
-    });    
+      console.log(foundDoctor);
+
+      docListTemp.push(foundDoctor);
+
+      // docListTemp2 = docListTemp.slice();
+
+      console.log(docListTemp);
+
+    });
 
     console.log(docListTemp);
     that.doctors.push(docListTemp);
     console.log(that.doctors);
-  
-}).fail(function(error){
-  console.log("Error completing request: " + error);
-});
+
+  }).fail(function (error) {
+    console.log("Error completing request: " + error);
+  });
 
 
 }
 
-export function Doctor(){
+export function Doctor() {
   this.firstName = "";
   this.lastName = "";
   this.address = "";
@@ -79,5 +71,3 @@ export function Doctor(){
   this.website = "";
   this.acceptsPatients = "";
 }
-
-
