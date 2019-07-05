f77c77219277a05795a7ed8bb8522be0d0675517
HoldenJC <holdenj_c@yahoo.com>
2019-07-05 15:23:35 -0700

    set up doctorList object

--- src/backend-code.js
+++ src/backend-code.js
@@ -1,12 +1,83 @@
-export class Example {
-  constructor(){
-    this.name = "Holden";
-    this.picture = "";
-  }
+import $ from 'jquery';
+let docListTemp = [];
+let docListTemp2 = [];
 
-  exampleMethod(){
-    this.name = "HoldenJC";
-  }
+export function DoctorList() {
+  this.doctors = [];
+}
+
+DoctorList.prototype.addDoctor = function(doc){
+  // debugger;
+  this.doctors = doc;
+  console.log(doc);
+}
+
+DoctorList.prototype.searchDoc = function(query){
+
+  // debugger;
+  let that = this;
+  // let docListTemp = [];
+
+  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=45.523064,-122.676483,50&skip=0&limit=10&user_key=${process.env.exports.apiKey}&query=${query}`).then(function(response){
+
+    // let that = this;
+    let docArray = response.data;
+    // let foundDocTemp;
+    console.log(docArray);
+    
+
+    docArray.forEach((doctor) => {
+      // if(doctor.within_search_area === true){
+        let foundDoctor = new Doctor();
+        if(doctor.profile.first_name !== undefined){
+          foundDoctor.firstName = doctor.profile.first_name;
+        }
+        if(doctor.profile.last_name !== undefined){
+          foundDoctor.lastName = doctor.profile.last_name;
+        }
+        // foundDoctor.address = doctor.visit_address;
+        // (doctor.phones).forEach(function(phoneNumber){
+        //   if(phoneNumber.type === "landline"){
+        //     foundDoctor.phone = phoneNumber.number;
+        //   }
+        // });
+        if(doctor.website !== undefined){
+          foundDoctor.website = doctor.website;
+        }
+        if(doctor.accepts_new_patients !== undefined){
+          foundDoctor.acceptsPatients = doctor.accepts_new_patients;
+        }
+        
+
+        console.log(foundDoctor);
+         
+        docListTemp.push(foundDoctor); 
+
+        docListTemp2 = docListTemp.slice();
+        
+        console.log(docListTemp);
+
+      // }
+    });    
+
+    console.log(docListTemp);
+    that.doctors.push(docListTemp);
+    console.log(that.doctors);
+  
+}).fail(function(error){
+  console.log("Error completing request: " + error);
+});
+
+
+}
+
+export function Doctor(){
+  this.firstName = "";
+  this.lastName = "";
+  this.address = "";
+  this.phone = "";
+  this.website = "";
+  this.acceptsPatients = "";
 }
 
 
--- src/main.js
+++ src/main.js
@@ -2,61 +2,60 @@ import $ from 'jquery';
 import './styles.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Example } from './backend-code';
+import { DoctorList } from './backend-code';
 
-let example = new Example();
-let firstNameArray = [];
-let lastNameArray = [];
-let middleNameArray = [];
+let doctorList = new DoctorList();
+// let firstNameArray = [];
+// let lastNameArray = [];
+// let addressArray = [];
+// let phoneArray = [];
+// let websiteArray = [];
+// let newPatients = [];
 let searchQuery = "";
 
+// function searchDoc(query){
 
-// https://api.betterdoctor.com/2016-03-01/doctors?&location=or-portland&skip=0&limit=10&user_key=f0628ccddc20bcbdf813d9fbfdcca1ce&query=acne
+//   $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=45.523064,-122.676483,50&skip=0&limit=10&user_key=${process.env.exports.apiKey}&query=${query}`).then(function(response){
+//     let docArray = response.data;
 
-
-function searchDoc(query){
-
-  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}&query=${query}`).then(function(response){
-    let docArray = response.data;
-
-    docArray.forEach(function(doctor){
-      firstNameArray.push(doctor.profile.first_name);
-
-      if(doctor.profile.middle_name === undefined){
-        middleNameArray.push("(no middle name)"); 
-      } else {
-        middleNameArray.push(doctor.profile.middle_name);
-      }
-
-      lastNameArray.push(doctor.profile.last_name);      
-    });
-    console.log(firstNameArray);
-    console.log(middleNameArray);
-    console.log(lastNameArray);
+//     docArray.forEach(function(doctor){
+//       if(doctor.within_search_area === true){
+//         firstNameArray.push(doctor.profile.first_name);
+//         lastNameArray.push(doctor.profile.last_name);
+//         addressArray.push(doctor.visit_address);
+//         (doctor.phones).forEach(function(phoneNumber){
+//           if(phoneNumber.type === "landline"){
+//             phoneArray.push(phoneNumber.number);
+//           }
+//         });
+//         websiteArray.push(doctor.)      
+//       }
+//     });
+//     // console.log(firstNameArray);
+//     // console.log(lastNameArray);
     
-  });
+//   }).fail(function(error){
+//     console.log("Error completing request: " + error);
 
-}
+//   });
+
+// }
 
 
 function addListeners(){
  
   $("#querySubmit").click(function(){
-    firstNameArray = [];
-    lastNameArray = [];
-    middleNameArray = [];
+    // firstNameArray = [];
+    // lastNameArray = [];
+    // middleNameArray = [];
     searchQuery = $("#querySearch").val();
     console.log(searchQuery);
-    searchDoc(searchQuery);
+    doctorList.searchDoc(searchQuery);
+    // searchDoc(searchQuery);
   });
 
 }
 
 $(document).ready(function() {
   addListeners();
-
-  $("#searchResult").append(example.name);
-  example.exampleMethod();
-  $("#searchResult").append(example.name);
-
 });
